bd498a5e8ecdf2b2e9a988942d3d6dd2a0b249ba
JaffarAliHassan <j3.bh07@gmail.com>
2025-04-09 15:44:22 +0300

    first commit

---
+++ Scripts/Login.js
@@ -0,0 +1,23 @@
+const UsernameField = document.getElementById('UserUsername');
+const passwordField = document.getElementById('UserPassword');
+const signUpButton = document.getElementById('LoginBtn');
+
+signUpButton.addEventListener('click', function() {
+
+    const Username = UsernameField.value;
+    const password = passwordField.value;
+
+    db.collection("Login").doc(Username)
+    db.collection("Login").doc(Username).set({
+        Username: Username,
+        Pass: password,
+    })
+    .then((docRef) => {
+        console.log("user added");
+        UsernameField.value = "";
+        passwordField.value = "";
+    })
+    .catch((error) => {
+        console.error("Error adding User: ", error);
+    });
+});
